763da86ac23a00ab4878bf49a13ba42d46274929
김승민 <winikim@MacBook-Pro.local>
2020-04-25 01:48:53 +0900

    change copyright

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -4,8 +4,8 @@ import './index.scss'
 
 export const Footer = () => (
   <footer className="footer">
-    ©<a href="https://github.com/JaeYeopHan">Jbee</a>, Built with{' '}
-    <a href="https://github.com/JaeYeopHan/gatsby-starter-bee">
+    ©<a href="https://github.com/winikim">winikim</a>, Built with{' '}
+    <a href="https://github.com/winikim/gwinikimy-starter-bee">
       Gatsby-starter-bee
     </a>
   </footer>
